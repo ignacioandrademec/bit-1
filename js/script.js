2f882fdce91dd20b7fabb083b268aad76b2401a5
Jose Andrade <ignacio.andrademec@gmail.com>
2025-05-12 11:22:10 -0500

    Cambios de form

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
 const $contactForm = document.getElementById("contactForm");
 $contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log($contactForm.name.value);
+    const name = $contactForm.name.value;
+    const email = $contactForm.email.value;
+    const message = $contactForm.message.value;
+
+    console.log(name)
+    console.log(email)
+    console.log(message)
+    
+    // console.log($contactForm.name.value);
+    // console.log($contactForm.email.value);
+    // console.log($contactForm.message.value);
+    
+    const result = ` ${name}, gracias, tu información fue cargada correctamente`;
+
+    alert(result);
+
+    $contactForm.reset ();
 
 });
