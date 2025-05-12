8704761ab1ccac057d097d7bb4f950c11b6b9f32
Jose Andrade <ignacio.andrademec@gmail.com>
2025-05-12 13:57:20 -0500

    Cargue final

--- js/script.js
+++ js/script.js
@@ -2,6 +2,40 @@
 
 // FORMULARIO SENCILLO
 
+const $contactForm2 = document.getElementById("contactForm2");
+$contactForm2.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = $contactForm2.name.value;
+    const email = $contactForm2.email.value;
+    const message = $contactForm2.message.value;
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+    
+    
+    const result = `Apreciado ${name}. 
+    
+Gracias por diligenciar la información.
+
+Lo invitamos a registrarse con su correo ${email}, en la sección Contáctenos, para cargar la información adicional de su empresa y su requerimiento.
+    
+Atenderemos su mensaje "${message}", lo más pronto posible, que tenga un excelente día.
+    
+Cordialmente, 
+    
+Jose Andrade
+Gerente de AG Polar Tech.`;
+
+    alert(result);
+
+    $contactForm2.reset ();
+
+});
+
+
+//FORMULARIO COMPLETO
+
 const $contactForm = document.getElementById("contactForm");
 $contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -28,7 +62,7 @@ Gracias por diligenciar la información, le informamos que su empresa ${nameEmpr
     
 Confirmamos que el área de servicio de ${selectorService} estará contactandolo en los proximos dias al correo registrado ${email}.
     
-Atenderemos su mensaje "${message}", lo mas pronto posible, que tenga un excelente día.
+Atenderemos su mensaje "${message}", lo más pronto posible, que tenga un excelente día.
     
 Cordialmente, 
     
