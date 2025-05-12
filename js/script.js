a0303e80158f5390397ea45c92deea0e5926fe46
Jose Andrade <ignacio.andrademec@gmail.com>
2025-05-12 13:36:16 -0500

    Javascript terminado

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ $contactForm.addEventListener("submit", (event) => {
     const nameNit = $contactForm.nameNit.value;
     const email = $contactForm.email.value;
     const selectorService = $contactForm.selectorService.value;
+    const checkSector = $contactForm.checkSector.value;
     const message = $contactForm.message.value;
 
     console.log(name);
@@ -17,21 +18,22 @@ $contactForm.addEventListener("submit", (event) => {
     console.log(nameNit);
     console.log(email);
     console.log(selectorService);
+    console.log(checkSector);
     console.log(message);
     
     
     const result = `Apreciado ${name}. 
     
-    Gracias por diligenciar la información, su empresa ${nameEmpresa} con NIT #${nameNit} fue registrada con éxito.
+Gracias por diligenciar la información, le informamos que su empresa ${nameEmpresa} del ${checkSector} con NIT #${nameNit}, fue registrada correctamente.
     
-    Confirmamos que el área de servicio de ${selectorService} estará contactandolo en los proximos dias al correo registrado ${email}.
+Confirmamos que el área de servicio de ${selectorService} estará contactandolo en los proximos dias al correo registrado ${email}.
     
-    Atenderemos su mensaje "${message}", lo mas pronto posible, que tenga un excelente día.
+Atenderemos su mensaje "${message}", lo mas pronto posible, que tenga un excelente día.
     
-    Cordialmente, 
+Cordialmente, 
     
-    Jose Andrade
-    Gerente de AG Polar Tech.`;
+Jose Andrade
+Gerente de AG Polar Tech.`;
 
     alert(result);
 
