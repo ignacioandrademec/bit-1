e1c46fcbe2281460291034b274610a1ce3264b0f
Jose Andrade <ignacio.andrademec@gmail.com>
2025-05-12 13:23:47 -0500

    Actualización Javascript

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
 "use strict";
+
+// FORMULARIO SENCILLO
+
 const $contactForm = document.getElementById("contactForm");
 $contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const name = $contactForm.name.value;
+    const nameEmpresa = $contactForm.nameEmpresa.value;
+    const nameNit = $contactForm.nameNit.value;
     const email = $contactForm.email.value;
+    const selectorService = $contactForm.selectorService.value;
     const message = $contactForm.message.value;
 
-    console.log(name)
-    console.log(email)
-    console.log(message)
+    console.log(name);
+    console.log(nameEmpresa);
+    console.log(nameNit);
+    console.log(email);
+    console.log(selectorService);
+    console.log(message);
     
-    // console.log($contactForm.name.value);
-    // console.log($contactForm.email.value);
-    // console.log($contactForm.message.value);
     
-    const result = ` ${name}, gracias, tu información fue cargada correctamente`;
+    const result = `Apreciado ${name}. 
+    
+    Gracias por diligenciar la información, su empresa ${nameEmpresa} con NIT #${nameNit} fue registrada con éxito.
+    
+    Confirmamos que el área de servicio de ${selectorService} estará contactandolo en los proximos dias al correo registrado ${email}.
+    
+    Atenderemos su mensaje "${message}", lo mas pronto posible, que tenga un excelente día.
+    
+    Cordialmente, 
+    
+    Jose Andrade
+    Gerente de AG Polar Tech.`;
 
     alert(result);
 
     $contactForm.reset ();
 
 });
+
